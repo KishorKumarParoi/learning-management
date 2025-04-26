31f85b5e8d9a9c708c584781232ae2f86efef276
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2025-04-26 16:54:07 +0600

    animation sequence like payment confirmation

--- client/src/app/motion/animation-sequence/page.tsx
+++ client/src/app/motion/animation-sequence/page.tsx
@@ -1,33 +1,100 @@
 'use client';
 
-import { motion, useAnimate } from 'motion/react';
+import { type AnimationSequence, motion, useAnimate } from 'motion/react';
 import AnimatedText from './AnimatedText';
-
 const AnimationSequence = () => {
   const [scope, animate] = useAnimate();
-  const startAnimating = () => {
-    animate(
-      '.text',
-      {
-        display: 'none',
-      },
-      {
-        duration: 0.1,
-      }
-    );
-    animate('button', { width: '0' }, { duration: 0.3 });
-    animate(
-      '.spinning-circle',
 
+  const sequence: AnimationSequence = [
+    ['.loader', { opacity: [0, 1], width: '2rem' }, { duration: 0.3 }],
+    ['.loader path', { pathLength: 1, rotate: 360 * 4 }, { duration: 1 }],
+    ['.loader', { opacity: [1, 0], width: '0%' }, { duration: 0.3 }],
+    ['.text', { display: 'none' }, { duration: 0.1, ease: 'easeInOut' }],
+    [
+      'button',
+      { width: '5rem', height: '5rem', borderRadius: '1200px' },
+      { duration: 0.3 },
+    ],
+    [
+      'button',
       {
-        opacity: 1,
-        scale: [0, 1.2, 1, 0.8, 1, 1.2, 1, 0.8, 1],
+        opacity: [1],
+        scale: [0.8, 1, 1.2, 1],
+        backgroundImage: 'linear-gradient(to right, #00ff99 , #00ccff)',
+        backdropFilter: 'blur(10px)',
       },
+      { duration: 1, ease: 'easeInOut' },
+    ],
+    ['.check-icon', { opacity: [0, 1] }, { duration: 0.1, at: '-0.5' }],
+    ['.check-icon path', { pathLength: 1 }, { duration: 0.5 }],
+    [
+      'button',
       {
-        duration: 1,
-        ease: 'easeInOut',
-      }
-    );
+        opacity: [1],
+        scale: [1, 1.2, 1],
+        backgroundImage: 'linear-gradient(to right, #00ff99 , #00ccff)',
+        backdropFilter: 'blur(10px)',
+      },
+      { duration: 1, ease: 'easeInOut', delay: 0.1 },
+    ],
+  ];
+
+  const startAnimating = async () => {
+    animate(sequence);
+    // await animate(
+    //   '.loader',
+    //   {
+    //     opacity: 1,
+    //     width: '2rem',
+    //   },
+    //   {
+    //     duration: 0.3,
+    //   }
+    // );
+    // await animate(
+    //   '.loader path',
+    //   { pathLength: 1, rotate: 360 * 4 },
+    //   { duration: 1 }
+    // );
+    // await animate(
+    //   '.loader',
+    //   {
+    //     opacity: 0,
+    //     width: '0%',
+    //   },
+    //   {
+    //     duration: 0.3,
+    //   }
+    // );
+    // animate(
+    //   '.text',
+    //   {
+    //     display: 'none',
+    //   },
+    //   {
+    //     duration: 0.1,
+    //   }
+    // );
+    // await animate(
+    //   'button',
+    //   { width: '5rem', height: '5rem', borderRadius: '1200px' },
+    //   { duration: 0.3 }
+    // );
+    // animate(
+    //   'button',
+    //   {
+    //     opacity: 1,
+    //     scale: [0.8, 1, 1.2, 1],
+    //     backgroundImage: 'linear-gradient(to right, #00ff99 , #00ccff)',
+    //     backdropFilter: 'blur(10px)',
+    //   },
+    //   {
+    //     duration: 1,
+    //     ease: 'easeInOut',
+    //   }
+    // );
+    // animate('.check-icon', { opacity: 1 }, { duration: 0.1 });
+    // animate('.check-icon path', { pathLength: 1 }, { duration: 0.5 });
   };
 
   return (
@@ -40,17 +107,52 @@ const AnimationSequence = () => {
         <motion.button
           onClick={() => startAnimating()}
           style={{ width: '30rem', height: '4rem' }}
-          className="text-white rounded-lg bg-gradient-to-r from-purple-500 via-violet-600 to-indigo-500 px-4 py-2 text-3xl font-bold shadow-lg transition-all duration-300 ease-in-out hover:scale-105 hover:shadow-xl active:scale-95"
+          className="text-white flex h-20 w-20 items-center justify-center rounded-lg bg-gradient-to-r from-purple-500 via-violet-600 to-indigo-500 text-3xl font-bold shadow-lg transition-all duration-300 ease-in-out hover:scale-105 hover:shadow-xl active:scale-95"
         >
+          <motion.svg
+            width={'24'}
+            height={'24'}
+            viewBox={'0 0 24 24'}
+            fill={'none'}
+            stroke={'currentColor'}
+            strokeWidth={2}
+            strokeLinecap={'round'}
+            strokeLinejoin={'round'}
+            className={'loader h-5 w-5 text-white-100'}
+            initial={{
+              width: '0%',
+            }}
+          >
+            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+            <path d="M12 3a9 9 0 1 0 9 9" />
+          </motion.svg>
           <span className="text">Purchase Now ($169) </span>
         </motion.button>
-        <motion.div
-          initial={{
+
+        <motion.svg
+          fill={'none'}
+          viewBox={'0 0 24 24'}
+          stroke={'#FFFFFF'}
+          strokeWidth={3}
+          className={
+            'check-icon pointer-events-none absolute inset-0 z-50 m-auto h-8 w-8'
+          }
+          style={{
             opacity: 0,
-            scale: 0,
           }}
-          className="spinning-circle absolute inset-0 m-auto h-20 w-20 rounded-full bg-green-700"
-        ></motion.div>
+        >
+          <motion.path
+            initial={{ pathLength: 0 }}
+            transition={{
+              duration: 0.5,
+              type: 'tween',
+              ease: 'easeOut',
+            }}
+            strokeLinecap={'round'}
+            strokeLinejoin={'round'}
+            d="M5 13l4 4L19 7"
+          />
+        </motion.svg>
       </div>
     </div>
   );
