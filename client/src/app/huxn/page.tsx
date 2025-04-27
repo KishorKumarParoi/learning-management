69146e7b9fd0b925e8d86db66d3bf30cf11cc43a
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2025-04-28 00:38:04 +0600

    Geastures animation

--- client/src/app/huxn/page.tsx
+++ client/src/app/huxn/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 import { motion } from 'motion/react';
+import Image from 'next/image';
 import { useState } from 'react';
 
 const cardVariants = {
@@ -11,11 +12,64 @@ const cardVariants = {
   },
 };
 
+const images = [
+  'https://images.unsplash.com/photo-1506748686214-e9df14d4d9d0?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwzNjUyOXwwfDF8c2VhcmNofDJ8fGZsYXRlJTIwY2FydHxlbnwwfHx8fDE2ODQ5NTk3MjA&ixlib=rb-4.0.3&q=80&w=400',
+  'https://images.unsplash.com/photo-1506748686214-e9df14d4d9d0?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwzNjUyOXwwfDF8c2VhcmNofDJ8fGZsYXRlJTIwY2FydHxlbnwwfHx8fDE2ODQ5NTk3MjA&ixlib=rb-4.0.3&q=80&w=400',
+  'https://images.unsplash.com/photo-1506748686214-e9df14d4d9d0?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwzNjUyOXwwfDF8c2VhcmNofDJ8fGZsYXRlJTIwY2FydHxlbnwwfHx8fDE2ODQ5NTk3MjA&ixlib=rb-4.0.3&q=80&w=400',
+  'https://images.unsplash.com/photo-1506748686214-e9df14d4d9d0?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwzNjUyOXwwfDF8c2VhcmNofDJ8fGZsYXRlJTIwY2FydHxlbnwwfHx8fDE2ODQ5NTk3MjA&ixlib=rb-4.0.3&q=80&w=400',
+  'https://images.unsplash.com/photo-1506748686214-e9df14d4d9d0?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwzNjUyOXwwfDF8c2VhcmNofDJ8fGZsYXRlJTIwY2FydHxlbnwwfHx8fDE2ODQ5NTk3MjA&ixlib=rb-4.0.3&q=80&w=400',
+];
+
 const Home = () => {
   const [isFlipped, setIsFlipped] = useState(false);
 
   return (
     <motion.div className="flex h-full items-center justify-center bg-slate-700 [perspective:1000px]">
+      <motion.div
+        initial={{
+          scale: 1,
+        }}
+        whileHover={{
+          scale: 1.1,
+          rotate: 3,
+        }}
+        whileTap={{
+          scale: 0.9,
+          rotate: -3,
+        }}
+        transition={{ type: 'spring', stiffness: 300, damping: 20 }}
+        drag
+        dragConstraints={{
+          top: -100,
+          left: -100,
+          right: 100,
+          bottom: 100,
+        }}
+        dragElastic={0.2}
+        dragTransition={{ bounceStiffness: 600, bounceDamping: 10 }}
+        className="mx-20 flex h-[500px] w-[500px] cursor-pointer flex-col items-center justify-center overflow-hidden rounded-lg bg-neutral-800 shadow-lg"
+      >
+        <Image
+          className="pointer-events-none w-1/2 object-contain"
+          src="https://images.unsplash.com/photo-1506748686214-e9df14d4d9d0?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwzNjUyOXwwfDF8c2VhcmNofDJ8fGZsYXRlJTIwY2FydHxlbnwwfHx8fDE2ODQ5NTk3MjA&ixlib=rb-4.0.3&q=80&w=400"
+          alt="Image"
+          width={200}
+          height={200}
+        />
+        <div className="bg-white-100 p-1 text-black">
+          <h2>Dynamic Card Title</h2>
+          <p>
+            amet natus a eius quisquam quam tempora porro id distinctio unde t
+            natus nostrum aut eos esse, reprehenderit perspiciatis. Odit ut
+            nulla repudiandae vel, cum architecto animi! Est maxime suscipit
+            cupiditate deleniti.
+          </p>
+          <button className="mt-4 rounded bg-blue-800 px-4 py-2 text-white-100">
+            Click Me
+          </button>
+        </div>
+      </motion.div>
+
       <motion.div
         variants={cardVariants}
         initial="front"
@@ -31,6 +85,7 @@ const Home = () => {
           Front Side
         </motion.div>
       </motion.div>
+
       <motion.div
         drag
         whileDrag={{
@@ -46,6 +101,30 @@ const Home = () => {
       >
         kkp
       </motion.div>
+
+      <div className="mx-20 flex w-[80%] gap-4">
+        {images.map((image, index) => (
+          <motion.div
+            key={index}
+            className="relative h-40 w-40 overflow-hidden rounded-lg shadow-lg"
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 0.9 }}
+          >
+            <Image
+              src={image}
+              alt={`Image ${index + 1}`}
+              fill
+              className="object-cover"
+            />
+            <motion.div
+              className="text-white absolute inset-0 flex cursor-pointer items-center justify-center bg-opacity-50 text-lg font-bold opacity-0 transition-opacity duration-200 hover:bg-black/70"
+              whileHover={{ opacity: 1 }}
+            >
+              <p className="text-lg">Image Caption</p>
+            </motion.div>
+          </motion.div>
+        ))}
+      </div>
     </motion.div>
   );
 };
