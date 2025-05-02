d378104c4d4a41ce1385c907605482c696542b30
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2025-05-02 19:03:12 +0600

    course controllers

---
+++ server/src/controllers/courseController.ts
@@ -0,0 +1,34 @@
+import { error } from "console";
+import { Request, Response } from "express";
+import Course from "../models/courseModel";
+
+export const listCourses = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  const { category } = req.query;
+  try {
+    const courses =
+      category && category !== "all"
+        ? await Course.scan("category").eq(category).exec()
+        : await Course.scan().exec();
+    res.json({ message: "Courses Received Successfully!", data: courses });
+  } catch (err) {
+    console.log(`Error Happening: ${err}`);
+    res.status(500).json({ message: "Error Retrieving courses", error });
+  }
+};
+
+export const getCourse = async (req: Request, res: Response): Promise<void> => {
+  const { courseId } = req.params;
+  try {
+    const course = await Course.get(courseId);
+    if (!course) {
+      res.status(404).json({ message: "Course Not Found" });
+      return;
+    }
+    res.json({ message: "Courses Received Successfully!", data: course });
+  } catch (err) {
+    res.status(500).json({ message: "Error Retrieving course", error });
+  }
+};
