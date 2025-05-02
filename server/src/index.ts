dc9aa33ba7d944ec54a9ff6af8e75ba0058b13d7
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2025-05-02 18:50:15 +0600

    setup backend, morgan, helmet, fixed issue of esm/commonjs

---
+++ server/src/index.ts
@@ -0,0 +1,47 @@
+import bodyParser from "body-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+import * as dynamoose from "dynamoose";
+import express from "express";
+import helmet from "helmet";
+import morgan from "morgan";
+
+// Route Imports
+
+// Configurations
+dotenv.config();
+
+const isProduction = process.env.NODE_ENV === "production";
+
+if (isProduction) {
+  dynamoose.aws.ddb.local();
+}
+
+const app = express();
+app.use(express.json());
+
+app.use(helmet());
+app.use(
+  helmet.crossOriginResourcePolicy({
+    policy: "cross-origin",
+  })
+);
+
+app.use(morgan("common"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+
+/* ROUTES */
+app.get("/", (req, res) => {
+  res.send("Hello World!\n");
+});
+
+/* SERVER */
+const port = process.env.PORT || 3000;
+
+if (!isProduction) {
+  app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+  });
+}
--- server/src/seed/seedDynamodb.ts
+++ server/src/seed/seedDynamodb.ts
@@ -1,16 +1,16 @@
 import {
-  DynamoDBClient,
   DeleteTableCommand,
+  DynamoDBClient,
   ListTablesCommand,
 } from "@aws-sdk/client-dynamodb";
+import dotenv from "dotenv";
+import dynamoose from "dynamoose";
 import fs from "fs";
 import path from "path";
-import dynamoose from "dynamoose";
 import pluralize from "pluralize";
-import Transaction from "../models/transactionModel";
-import Course from "../models/courseModel";
-import UserCourseProgress from "../models/userCourseProgressModel";
-import dotenv from "dotenv";
+import Course from "../models/courseModel.js";
+import Transaction from "../models/transactionModel.js";
+import UserCourseProgress from "../models/userCourseProgressModel.js";
 
 dotenv.config();
 let client: DynamoDBClient;
